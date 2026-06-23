c582b1b2e602c2c755c874246f9216fd533142ab
jainsnehaa28-design <jainsnehaa28@gmail.com>
2026-06-23 22:38:31 +0530

    Add footer component

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-darkBrown text-white py-12 px-4">
+      <div className="max-w-5xl mx-auto text-center">
+        <h3 className="text-2xl font-elegant mb-4">Thank You!</h3>
+        <p className="mb-4">We can't wait to celebrate with you!</p>
+        <p className="text-sm text-gray-400">
+          © 2024 Our Wedding. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
